af24bd29d694d2a9301b8f4e02bac886cee82cc6
fucking ajeet <ajeet808080@gmail.com>
2022-09-12 23:48:43 +0530

    nothing much

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 // #include<bits/stdc++.h>
 #include<vector>
 #include<algorithm>
@@ -59,6 +60,7 @@ using namespace std;
 
 
 
+
 void init_code(){
    
     #ifndef ONLINE_JUDGE
@@ -70,22 +72,36 @@ void init_code(){
 int main() {
    fast_io;
    init_code();
-
-
-   string s,k;
-   cin>>s;
-   for(int i =0; i<sl; i++)
+   int tt;
+   cin>>tt;
+   while(tt--)
    {
-   	if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' && s[i]!='y' && s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U' && s[i]!='Y')
-		{
-		k =k +'.';
-		k  =k +(char) tolower(s[i]);
+   	int a,b,c;
+   	cin>>a>>b>>c;
+   	int time1 = a-1;
+   	int time2= 0;
+   	if(b>c)
+   	{
+   		time2 = b-1;
 
-		}
-
+   	}
+   	else
+   	{
+   		time2= abs(c-b)+c-1;
+   	}
+   	if(time1<time2)
+   	{
+   		cout<<1<<endl;
+   	}
+   	else if(time1>time2)
+   	{
+   		cout<<2<<endl;
+   	}
+   	else
+   	{
+   		cout<<3<<endl;
+   	}
+    
    }
-   cout<<k<<endl;
-
-
    return 0;
 }
