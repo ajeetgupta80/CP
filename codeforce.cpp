8aeabfa134605ee7e822a147bc8e3b35ee7a0a83
fucking ajeet <ajeet808080@gmail.com>
2022-11-13 23:56:15 +0530

    nothing much

--- codeforce.cpp
+++ codeforce.cpp
@@ -124,23 +124,75 @@ void printv(vector<int> &v)
 
 
 
-
+// int fact[1000001];
 void solve()
-{   
-   int n;
-   cin>>n;
-   int ans = n/2;
-   if(n%2==0)
-   {
-      cout<<ans<<nline;
-   }
-   else
-      cout<<ans+1<<nline;
-    
-    
+{  
+   // long long n;
+   // cin>>n;
+   // string s;
+   // cin>>s;
+   // long long ans =0;
+   // long long dist=0,maxf=0,c[10];
+   
+   // for(int i=0; i<n; ++i)
+   // {
+   //    for(int j=0; j<9; ++j)
+   //    {
+   //       c[j]=0;
+   //    }
+   //    dist =0;
+   //    maxf =0;
 
+   //    for(int j=i; j<=i+99 && j<n; ++j)
+   //    {
+   //       c[s[i]-'0']++;
+   //       maxf=max(maxf,c[s[i]-'0']);
+   //       if(c[s[i]-'0']==1)
+   //       {
+   //          dist++;
+   //       }
+         
+   //       if(dist>= maxf)
+   //          ans++;
 
-}
+   //    }
+   // }
+   // cout<<ans<<nline;
+
+    ll t,ans, n, i, j, t1, l, maxf;
+    string a;
+    
+        cin>>n;
+        
+        cin>>a;
+        ans=0;
+        
+        ll c[10], dis=0;
+        
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<=9; j++){
+                c[j]=0;
+            }
+            maxf=0;
+            dis=0;
+            
+            for(j=i; j<=i+99 && j<n; j++)
+            {
+                c[a[j]-'0']++;
+                maxf=max(maxf, c[a[j]-'0']);
+                if(c[a[j]-'0']==1){
+                    dis++;
+                }
+                
+                if(dis>=maxf)
+                ans++;
+            }
+        }
+        
+        cout<<ans<<"\n";
+    }
+
 
 // int main() 
 
@@ -154,7 +206,6 @@ signed main()
    while(tt--)
    {
       solve();
-     
    } 
   
 
